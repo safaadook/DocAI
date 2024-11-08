38848b8688394b0e0791c9a75514f5225acad0ef
ritvikthota08 <122498424+ritvikthota08@users.noreply.github.com>
2024-11-07 19:21:33 -0600

    Home Page
    Started the home page

---
+++ docai/src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//import { useNavigate } from 'react-router-dom';
+import "./Home.css";
+
+const Home = () => {
+  //const navigate = useNavigate();
+
+  return (
+    <div>
+    <h1>Chat with DocAI</h1>
+    <div className = "buttons"></div>
+    <button> GET STARTED </button>
+    <p> </p>
+    <button> LEARN MORE </button>
+    </div>
+
+  );
+};
+
+export default Home;  // Make sure this export is present
